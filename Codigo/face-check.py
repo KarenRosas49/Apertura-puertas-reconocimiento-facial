f59e0ca222c01108346962bf6bbcce2d772cd73d
Karen Rosas <rosas.karen0926@gmail.com>
2022-09-26 10:23:37 -0500

    imagenes similares

--- Codigo/face-check.py
+++ Codigo/face-check.py
@@ -1,6 +1,6 @@
 from deepface import DeepFace
 
-result = DeepFace.verify(img1_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/Imagen/face.jpg", img2_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/Imagen/Foto2.jpg")
+result = DeepFace.verify(img1_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/Imagen/timothee1.jpeg", img2_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/Imagen/timothee2.jpeg")
 
 print("Resultados")
 print(result)
