c510e463b4b2d82cc3788fdf35a93a433889f7dc
Karen Rosas <rosas.karen0926@gmail.com>
2022-09-28 10:36:20 -0500

    Ejemplo

--- Codigo/face-recognition.py
+++ Codigo/face-recognition.py
@@ -2,7 +2,7 @@ from deepface import DeepFace
 
 print("Buscando rostro")
 
-df = DeepFace.find(img_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/Imagen/timothee.jpeg", db_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/my_db")
+df = DeepFace.find(img_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/Imagen/Adam.jpeg", db_path = "/home/karen/Documents/GitHub/Apertura-puertas-reconocimiento-facial/my_db")
 
 print("Resultado ")
 print(df)
