a556a2ee2cea9ae451487362cc8c02873981a6b2
Karen Rosas <rosas.karen0926@gmail.com>
2022-10-03 09:58:15 -0500

    mqtt

--- Codigo/MQTT/mqtt-sub.py
+++ Codigo/MQTT/mqtt-sub.py
@@ -1,4 +1,35 @@
 # Biblioteca
-import paho.mqtt.client as mqtt
+import paho.mqtt import client as mqtt_client
+import random
 
-def on_connect(client, userdata, flags)
+# Datos del broker
+broker = '3.122.60.139'
+port = 1883
+topic = "codigoIoT/mqtt/python"
+client_id = f'python-mqtt-{random.randit(0,1000)}'
+
+# Conexion al broker
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+# Subscripcion
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+# Opcion A
+client = connect_mqtt()
+subscribe(client)
+client.loop_forever()
